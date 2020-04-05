d830597f41c7f7d6bbd0ba0eaef21ab48d24f81e
Srikanth SM <srikanthsm@Admins-MacBook-Air.local>
2020-04-05 12:38:44 +0530

    add O

---
+++ 2020.04/2020.04.05 - Olymp GraphRepresentation/TaskO.java
@@ -0,0 +1,31 @@
+package cp;
+
+import java.util.Scanner;
+import java.io.PrintWriter;
+
+public class TaskO {
+    public void solve(int testNumber, Scanner in, PrintWriter out) {
+        String[] s = in.nextLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
+        int grid[][] = new int[n][n];
+        while (m-- > 0) {
+            String g[] = in.nextLine().split(" ");
+            int a = Integer.parseInt(g[0]);
+            int b = Integer.parseInt(g[1]);
+            --a;
+            --b;
+            grid[a][b] = 1;
+            grid[b][a] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    count++;
+                }
+            }
+            System.out.print(count+" ");
+    }
+}
